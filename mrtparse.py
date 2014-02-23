0c043f2163a83bf4cbcb7a1b7e78f329f6580c9b
Yoshiyuki Yamauchi <y.44snow@gmail.com>
2014-02-23 22:37:42 +0900

    Update mrtparse.py

--- mrtparse.py
+++ mrtparse.py
@@ -15,7 +15,7 @@ import gzip, bz2
 
 # mrtparse information
 __pyname__  = 'mrtparse'
-__version__ =  '0.9'
+__version__ =  '1.0'
 __descr__   = 'parse a MRT-format data'
 __url__     = 'https://github.com/YoshiyukiYamauchi/mrtparse'
 __author__  = 'Tetsumune KISO, Yoshiyuki YAMAUCHI, Nobuhiro ITOU'
