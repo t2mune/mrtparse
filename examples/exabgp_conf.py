72fb9e52a98853ee85b17e82fdd120968f029938
t2mune <t2mune@gamil.com>
2014-04-20 06:54:26 +0900

    Update - state the Apache license 2.0 explicitly

--- examples/exabgp_conf.py
+++ examples/exabgp_conf.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 '''
-exabgp_conf.py is a script to create a configuration file for exabgp using mrtparse.
+exabgp_conf.py - a script to create a configuration file for exabgp using mrtparse.
 
-See https://github.com/YoshiyukiYamauchi/mrtparse for more informations.
-This script is published under a Apache License, Version 2.0.
-Created by
-    Tetsumune KISO <t2mune@gmail.com>,
-    Yoshiyuki YAMAUCHI <info@greenhippo.co.jp>,
-    Nobuhiro ITOU <js333123@gmail.com>.
-Copyright (C) greenHippo, LLC. All rights reserved.
+Copyright (C) 2014 greenHippo, LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Authors:
+    Tetsumune KISO <t2mune@gmail.com>
+    Yoshiyuki YAMAUCHI <info@greenhippo.co.jp>
+    Nobuhiro ITOU <js333123@gmail.com>
 '''
 
 from mrtparse import *
--- mrtparse.py
+++ mrtparse.py
@@ -1,13 +1,24 @@
 '''
-mrtparse is a module to parse a MRT format data.
+mrtparse.py - MRT format data parser
 
-See https://github.com/YoshiyukiYamauchi/mrtparse for more informations.
-This module is published under a Apache License, Version 2.0.
-Created by
-    Tetsumune KISO <t2mune@gmail.com>,
-    Yoshiyuki YAMAUCHI <info@greenhippo.co.jp>,
-    Nobuhiro ITOU <js333123@gmail.com>.
-Copyright (C) greenHippo, LLC. All rights reserved.
+Copyright (C) 2014 greenHippo, LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Authors:
+    Tetsumune KISO <t2mune@gmail.com>
+    Yoshiyuki YAMAUCHI <info@greenhippo.co.jp>
+    Nobuhiro ITOU <js333123@gmail.com>
 '''
 
 import sys, struct, socket
