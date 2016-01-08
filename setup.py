ae8602d646393f31e37e99cb1af7e36535efb268
t2mune <t2mune@gmail.com>
2016-01-08 22:08:36 +0900

    Fix - can't install with distutils.

--- setup.py
+++ setup.py
@@ -1,41 +1,56 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(
-    name='mrtparse',
+import os
+from codecs import open
+import mrtparse
 
-    version='1.4',
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_descr = f.read()
 
-    description='mrtparse is a module to read and analyze the MRT format data.',
-
-    url='https://github.com/YoshiyukiYamauchi/mrtparse',
-
-    author='Tetsumune KISO, Yoshiyuki YAMAUCHI, Nobuhiro ITOU',
-
-    author_email='t2mune@gmail.com, info@greenhippo.co.jp, js333123@gmail.com',
-
-    license='Apache License 2.0',
-
-    platforms='any',
-
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: System :: Networking',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-    ],
-
-    keywords=['mrt', 'bgp'],
-
-    packages=['mrtparse'],
-
-    include_package_data=True,
-
-    use_2to3=True,
-)
+try:
+    os.symlink('../examples', 'mrtparse/examples')
+    os.symlink('../samples', 'mrtparse/samples')
+    setup(
+        name=mrtparse.__name__,
+        version=mrtparse.__version__,
+        description='MRT format data parser',
+        long_description=long_descr,
+        url='https://github.com/YoshiyukiYamauchi/mrtparse',
+        author='Tetsumune KISO, Yoshiyuki YAMAUCHI, Nobuhiro ITOU',
+        author_email='t2mune@gmail.com, info@greenhippo.co.jp, js333123@gmail.com',
+        license='Apache License 2.0',
+        platforms='any',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'Intended Audience :: Telecommunications Industry',
+            'Operating System :: OS Independent',
+            'Topic :: System :: Networking',
+            'License :: OSI Approved :: Apache Software License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Topic :: Internet',
+        ],
+        keywords='mrt bgp',
+        packages = ['mrtparse'],
+        package_data={
+            'mrtparse': [
+                'examples/*.py',
+                'samples/bird*',
+                'samples/openbgpd*',
+                'samples/quagga*'
+            ]
+        },
+        include_package_data=True,
+        use_2to3=True,
+        zip_safe=False,
+    )
+finally:
+    os.unlink('mrtparse/examples')
+    os.unlink('mrtparse/samples')
