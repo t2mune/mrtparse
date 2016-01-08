0556fa3ee980a57319c9acbcf9e51402ef87bbcc
t2mune <t2mune@gmail.com>
2016-01-08 22:11:00 +0900

    Fix - can't install with distutils.

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
+'''
+setup.py - a setup script
+
+Copyright (C) 2016 greenHippo, LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Authors:
+    Tetsumune KISO <t2mune@gmail.com>
+    Yoshiyuki YAMAUCHI <info@greenhippo.co.jp>
+    Nobuhiro ITOU <js333123@gmail.com>
+'''
+
 try:
     from setuptools import setup
 except ImportError:
